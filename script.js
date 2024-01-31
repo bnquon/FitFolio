88f2f48648c03e5d86bffbeae67ee2f61c71ae1f
Brandon Quon <bqa7@sfu.ca>
2024-01-30 22:14:31 -0800

    Implemented the switching overlay and changed the message on switch !! THUMBS UP

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
-const overlay = document.getElementById("overlay")
+const overlay = document.getElementById("overlay");
+const login = document.getElementById("login-form");
+const signup = document.getElementById("signup-form")
 const switchBtn = document.getElementById("switchBtn");
-const overlayMsg = document.getElementById("overlayMsg");
+
+const signupMsg = document.getElementById("signupMsg");
+const loginMsg = document.getElementById("loginMsg");
 
 switchBtn.onclick = function () {
     // if (overlayMsg.textContent === "Hello") {
     //     overlayMsg.textContent = "Bye";
     // };
-
     overlay.classList.toggle("show");
+    signup.classList.toggle("show");
+    login.classList.toggle("show");
+
+    
+
+    if (loginMsg.classList.contains('hidden')) {
+        loginMsg.classList.remove('hidden');
+        signupMsg.classList.add('hidden');
+        switchBtn.textContent = "Login";
+    } else {
+        signupMsg.classList.remove('hidden');
+        loginMsg.classList.add('hidden');
+        switchBtn.textContent = "Sign Up";
+    };
+
 }

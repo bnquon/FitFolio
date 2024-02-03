7f1369b2f8aa317751bacd44bd9b485f1960e3ca
Brandon Quon <bqa7@sfu.ca>
2024-02-02 19:41:50 -0800

    Goin to work on table functionality

--- cardio.js
+++ cardio.js
@@ -7,14 +7,8 @@ function addRow() {
     var cell3 = row.insertCell(2);
     var cell4 = row.insertCell(3);
     var cell5 = row.insertCell(4);
-    cell1.innerHTML = "NEW CELL1";
-    cell2.innerHTML = "NEW CELL2";
-    cell3.innerHTML = "NEW CELL2";
-    cell4.innerHTML = "NEW CELL2";
-    cell5.innerHTML = "NEW CELL2";
 }
 
 function deleteRow() {
     document.getElementById("tracker-sheet").deleteRow(1);
 }
-

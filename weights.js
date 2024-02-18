03b48d493c8bb1ea43e7efa2c3c6ca70e71f6403
Brandon Quon <bqa7@sfu.ca>
2024-02-17 19:35:47 -0800

    Added month logic to change year based on current month value

--- weights.js
+++ weights.js
@@ -128,43 +128,11 @@ function addElementToGrid(content, title) {
    gridContainer.appendChild(newElement);
 }
 
+let currentMonth = 0;
+
 function daysInMonth(month, year) {
     return new Date(year, month + 1, 0).getDate();
 }
-
-// function generateCalendar() {
-//     const table = document.getElementById("calendar");
-//     const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-
-//     var date = new Date();
-//     var month = date.getMonth();
-//     var year = date.getFullYear();
-//     console.log(month);
-//     console.log(year);
-
-//     var firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
-//     let dayAbbreviation = firstDay.toLocaleString('en-US', { weekday: 'short' });
-
-//     let numDay = daysInMonth(month, year);
-//     console.log(numDay);
-
-//     // Find the index of the first day
-//     let startIndex = daysOfWeek.indexOf(dayAbbreviation);
-
-//     for (let i = 0; i < 6; i++) {
-//         let row = table.insertRow();
-//         for (let j = 0; j < 7; j++) {
-//             let cell = row.insertCell();
-//             calendarCellStyle(cell);
-//             if (i === 0 && j < startIndex) {
-//                 cell.textContent = "";
-//             } else if (i*7 + j - startIndex + 1 <= numDay) {
-//                 cell.textContent = i*7 + j - startIndex + 1;
-//             } else i++;
-//         }
-//     }
-// }
-let currentMonth = 0;
 
 function generateCalendar(monthChange) {
     const table = document.getElementById("calendar");
@@ -174,6 +142,13 @@ function generateCalendar(monthChange) {
     var date = new Date();
     var month = date.getMonth() + monthChange;
     var year = date.getFullYear();
+    if (month > 11) {
+        month = 0;
+        year += 1;
+    } else if (month < 0) {
+        month = 11;
+        year -= 1;
+    }
 
     const monthNames = [
         "January",

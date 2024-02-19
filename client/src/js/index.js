5d629097111daf45e0e539f2e4f1f2f6a112b7c8
Brandon Quon <bqa7@sfu.ca>
2024-02-19 15:54:46 -0800

    pain 1

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,50 +1,95 @@
-// JS FOR MAIN PAGE (index.html)
-const addressUser = sessionStorage.getItem('username');
-document.getElementById('username').textContent = addressUser;
-document.getElementById('username').style.fontWeight = '700';
+// LOGIN PAGE
+const overlay = document.getElementById("overlay");
+const login = document.getElementById("login-form");
+const signup = document.getElementById("signup-form");
+const switchBtn = document.getElementById("switchBtn");
 
-function chooseQuote() {
-    var quotes = [
-        `“We are what we repeatedly do. Excellence then is not an act but a habit.” -Aristotle`,
-        `“Success is not final; failure is not fatal: It is the courage to continue that counts.” -Winston Churchill`,
-        `“The body achieves what the mind believes.” -Napoleon Hill`,
-        `“Dead last finish is greater than did not finish, which trumps did not start.” -Unknown`,
-        `“All progress takes place outside the comfort zone.” -Michael John Bobak`,
-        `“Motivation is what gets you started. Habit is what keeps you going.” -Jim Ryun`
-    ]
+const signupMsg = document.getElementById("signupMsg");
+const loginMsg = document.getElementById("loginMsg");
 
-    var quoteNum = Math.floor(Math.random() * quotes.length);
-    console.log(quoteNum);
-    console.log(quotes[quoteNum]);
-    typing(quotes[quoteNum], 0);
+switchBtn.onclick = function () {
+    overlay.classList.toggle("show");
+    signup.classList.toggle("show");
+    login.classList.toggle("show");
 
-}
+    if (loginMsg.classList.contains('hidden')) {
+        loginMsg.classList.remove('hidden');
+        signupMsg.classList.add('hidden');
+        switchBtn.textContent = "Login";
+    } else {
+        signupMsg.classList.remove('hidden');
+        loginMsg.classList.add('hidden');
+        switchBtn.textContent = "Sign Up";
+    }
+};
 
-document.addEventListener("DOMContentLoaded", (event) => {
-    chooseQuote();
+signup.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const newUsername = document.getElementById('usernameSignup').value;
+    const newPassword = document.getElementById('passwordSignup').value;
+    const serverURL = "mysql-fitfolio-bnquon-fitfolio.a.aivencloud.com";
+
+    fetch(`${serverURL}/createUser`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            name: newUsername,
+            password: newPassword,
+        }),
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+        if (data.createUserSuccessful) {
+            sessionStorage.setItem('username', newUsername);
+            sessionStorage.setItem('userid', data.userid);
+            window.location.href = "client/index.html";
+        }
+    })
+    .catch(error => {
+        console.error('Signup failed: ', error);
+    });
 });
 
-function typing(txt, position) {
-    if (position < txt.length) {
-        document.getElementById("inspire").innerHTML += txt.charAt(position);
-        position++;
-        setTimeout(function () {
-            typing(txt, position);
-        }, 35);
-    }
-}
 
-const cardioBtn = document.getElementById("cardio");
-const weightliftingBtn = document.getElementById("weightlifting");
-const cardioPic = document.getElementById("cardioBg");
-const weightPic = document.getElementById("weightBg");
+login.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const username = document.getElementById('usernameLogin').value;
+    const password = document.getElementById('passwordLogin').value; 
+    const serverURL = "mysql-fitfolio-bnquon-fitfolio.a.aivencloud.com";
 
-weightliftingBtn.addEventListener("mouseover", function() {
-    weightPic.style.display = "block";
-    cardioPic.style.display = "none";
-})
-
-cardioBtn.addEventListener("mouseover", function() {
-    weightPic.style.display = "none";
-    cardioPic.style.display = "block";
-})
+    fetch(`${serverURL}/login`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            name: username, 
+            password: password,
+        }),
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+        if (data.loginSuccessful) {
+            sessionStorage.setItem('username', username);
+            sessionStorage.setItem('userid', data.userid);
+            window.location.href = "index.html";
+        }
+    })
+    .catch(error => {
+        console.error('Login failed: ', error);
+    });
+});
